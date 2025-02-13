648a48b9c9a0af3f04beeb89e3fa3d10f26ccb98
Maks <84921780+MoveZZZZ@users.noreply.github.com>
2025-02-13 18:53:39 +0100

    Add features selection

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from DataPreprocessing import DataPreprocessing
 
 def main():
     data_preproc = DataPreprocessing("test")
-    data_preproc.change_data_labels(data_preproc.data_full.copy())
-
+    data_preproc.change_data_labels(data_preproc.read_data(data_preproc.base_dataset_path).copy())
+    data_preproc.refractoring_and_save_features_dataset()
 
 main()
