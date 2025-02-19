e557435d1b5a08f197470a181488013fe5d69553
Maks <84921780+MoveZZZZ@users.noreply.github.com>
2025-02-19 18:59:42 +0100

    Added LogisticRegression model for train

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from TrainModels import TrainModels
 from DataPreprocessing import DataPreprocessing
 
 def main():
-    #RFC, lgbm, xgb, mlp, tabnet, deep_mlp_3, deep_mlp_5, gnn, rbfn
+    #RFC, lgbm, xgb, mlp, tabnet, log_reg, deep_mlp_3, deep_mlp_5, gnn, rbfn
     #option, model_name, _activation_function, _optimizer, _epochs = 1, _num_centres=10
     log_filename = "AE_test_relu_selu_adam_sgd"
     _TrainModels = TrainModels(log_filename)
